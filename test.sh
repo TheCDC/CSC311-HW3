d97153c88b7cf2d2db8ee7a33410bcc85bd37a1c
Christopher Chen <christopher.chen1995@gmail.com>
2017-10-01 17:32:07 -0700

    Formatting

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 for FILE in $(find `pwd` -name in*.txt)
 do
 	if cat $FILE | java RDPExample1 | grep -q 'Successful'; then 
-		echo "Success"
+		echo "Success" $FILE 
 	else 
-		echo "FAIL"
+		echo "FAIL" $FILE
 	fi
 
 done
